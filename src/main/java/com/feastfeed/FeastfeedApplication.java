b8314254400bf0e467e0f7cbed16e738541b54a3
westd5 <77344568+westd5@users.noreply.github.com>
2023-06-15 20:11:02 -0400

    Initial Commit
    First commit for project generated using Spring Boot

---
+++ src/main/java/com/feastfeed/FeastfeedApplication.java
@@ -0,0 +1,13 @@
+package com.feastfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeastfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeastfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feastfeed/feastfeed/FeastfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feastfeed.feastfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeastfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
