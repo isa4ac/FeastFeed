50a61ddcf33eb71da9d9a69e4d77e040388daaad
ellaTerselic <terseler@mail.uc.edu>
2023-06-25 11:41:16 -0400

    partial progress - ella
    partial progress - ella

--- src/main/java/com/feastfeed/FeastfeedController.java
+++ src/main/java/com/feastfeed/FeastfeedController.java
@@ -1,10 +1,14 @@
 package com.feastfeed;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.feastfeed.dto.RecipeDTO;
 import com.feastfeed.dto.UserDTO;
@@ -15,7 +19,7 @@ import com.feastfeed.service.IUserService;
 public class FeastfeedController {
 	
 	@Autowired
-	private IRecipeService recipeServiceStub;
+	private IRecipeService recipeService;
 	
 	@Autowired
 	private IUserService userServiceStub;
@@ -37,10 +41,49 @@ public class FeastfeedController {
 		return "index";
 	}
 	
+	@RequestMapping (value="/start", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	@ResponseBody
+	public RecipeDTO readJSON(Model model) {
+		RecipeDTO recipeDTO = recipeService.fetchById(1);
+		model.addAttribute("recipeDTO", recipeDTO);
+		return recipeDTO;
+		
+	}
+	
 	@RequestMapping(value="/start", method=RequestMethod.GET)
 	public String readRecipe(Model model) {
-		RecipeDTO recipeDTO = recipeServiceStub.fetchById(1);
+		model.addAttribute("recipeDTO", new RecipeDTO());
+		return "start";
+	}
+	
+	@RequestMapping (value="/addRecipe", method=RequestMethod.GET)
+	public String addRecipe(Model model, @RequestParam(value="recipeTitle", required=false, defaultValue = "") String recipeTitle) 
+	{
+		RecipeDTO recipeDTO = recipeService.fetchById(1);
+		recipeDTO.setRecipeTitle(recipeTitle);
 		model.addAttribute("recipeDTO", recipeDTO);
+		return "start";
+	}
+	
+	@RequestMapping (value="/saverecipe")
+	public String saveRecipe(RecipeDTO recipeDTO) {
+		recipeDTO.setRecipeId(12);
+		return "start";
+	}
+	
+	@RequestMapping("/searchRecipes")
+	public String searchRecipes(@RequestParam(value="searchTerm", required=false, defaultValue = "") String searchTerm) 
+	{
+		String enhancedTerm = searchTerm + "";
+		recipeService.fetchRecipes(searchTerm);
+		return "start";
+	}
+	
+	@RequestMapping("/searchRecipesAll")
+	public String searchRecipesAll(@RequestParam Map<String,String> requestParams) 
+	{
+		int params = requestParams.size();
+		requestParams.get("searchTerm");
 		return "start";
 	}
 	
--- src/main/java/com/feastfeed/service/RecipeServiceStub.java
+++ src/main/java/com/feastfeed/service/RecipeServiceStub.java
@@ -1,28 +1,34 @@
 package com.feastfeed.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.feastfeed.dao.IRecipeDAO;
 import com.feastfeed.dto.RecipeDTO;
 
 @Component
 public class RecipeServiceStub implements IRecipeService {
 
+	private IRecipeDAO recipeDAO;
+	
+	ArrayList<String> stepsList = new ArrayList<>();
+	ArrayList<String> ingredientsList = new ArrayList<>();
+	
 	@Override
 	public RecipeDTO fetchById(int id) {
 		RecipeDTO recipeDTO = new RecipeDTO();
 		recipeDTO.setRecipeId(1);
 		recipeDTO.setRecipeTitle("Make a Peanut Butter and Jelly Sandwich");
 		
-		ArrayList<String> ingredientsList = new ArrayList<>();
 		ingredientsList.add("bread");
 		ingredientsList.add("peanut butter");
 		ingredientsList.add("jelly");
 		
 		recipeDTO.setRecipeIngredients(ingredientsList);
 		
-		ArrayList<String> stepsList = new ArrayList<>();
 		stepsList.add("Get two slices of bread");
 		stepsList.add("Spread  thin layer of peanut butter on one");
 		stepsList.add("Spread a layer of jelly on the other slice of bread");
@@ -34,7 +40,23 @@ public class RecipeServiceStub implements IRecipeService {
 	}
 	
 	@Override
-	public void save(RecipeDTO recipeDTO) {
+	public boolean save(RecipeDTO recipeDTO) throws Exception {
+		boolean result = recipeDAO.save(recipeDTO);
+		return result;
+	}
+
+	@Override
+	public List<RecipeDTO> fetchRecipes(String searchTerm) {
 		
+		List<RecipeDTO> matchingRecipes = new ArrayList<RecipeDTO>();
+		
+		if (searchTerm.contains("eanut") || searchTerm.contains("elly")) {
+			RecipeDTO recipe = new RecipeDTO();
+			recipe.setRecipeTitle("Make a Peanut Butter and Jelly Sandwich");
+			recipe.setRecipeSteps(stepsList);
+			recipe.setRecipeIngredients(ingredientsList);
+			matchingRecipes.add(recipe);
+		}
+		return matchingRecipes;
 	}
 }
