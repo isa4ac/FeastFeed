2a31857898580c8708f90adec07d4f9b6a3dbed4
isa4ac <56422223+isa4ac@users.noreply.github.com>
2023-06-25 13:03:13 -0400

    progress save

--- src/main/java/com/feastfeed/FeastfeedController.java
+++ src/main/java/com/feastfeed/FeastfeedController.java
@@ -3,8 +3,11 @@ package com.feastfeed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.feastfeed.dto.RecipeDTO;
 import com.feastfeed.dto.UserDTO;
@@ -30,11 +33,32 @@ public class FeastfeedController {
 		return "start";
 	}
 	
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public String read(Model model) {
-		UserDTO userDTO = userServiceStub.fetchById(01);
+	@RequestMapping("/saveuser") // needs tests to function?
+	public String saveUser(UserDTO userDTO) {
+		userDTO.setUserId(1010); // TO-DO replace with auto generated ID
+		return "index";
+	}
+	
+	@RequestMapping(value="/adduser", method=RequestMethod.GET)
+	public String addUser(Model model, @RequestParam(value="firstname", required=false, defaultValue ="") String firstName) {
+		UserDTO userDTO = userServiceStub.fetchById(1010);
+		userDTO.setFirstName(firstName);
 		model.addAttribute("userDTO", userDTO);
 		return "index";
+	}
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String read(Model model) {
+		model.addAttribute("userDTO", new UserDTO());
+		return "index";
+	}
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET, headers={"content-type=text/json"})
+	@ResponseBody
+	public UserDTO readJSON(Model model) {
+		UserDTO userDTO = userServiceStub.fetchById(1010);
+		model.addAttribute("userDTO", userDTO);
+		return userDTO;
 	}
 	
 	@RequestMapping(value="/start", method=RequestMethod.GET)
