6372cb85a0cb4720e195156982baa2521c66428e
isa4ac <56422223+isa4ac@users.noreply.github.com>
2023-07-09 23:10:01 -0400

    Reconstructed recipeDTO with new Ingredient object

--- src/main/java/com/feastfeed/dto/RecipeDTO.java
+++ src/main/java/com/feastfeed/dto/RecipeDTO.java
@@ -2,13 +2,38 @@ package com.feastfeed.dto;
 
 import java.util.ArrayList;
 
+class Ingredient {
+	private String name; // chopped tomato
+	private int unitCount; // 2 
+	private String unitName; // tablespoons
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getUnitCount() {
+		return unitCount;
+	}
+	public void setUnitCount(int unitCount) {
+		this.unitCount = unitCount;
+	}
+	public String getUnitName() {
+		return unitName;
+	}
+	public void setUnitName(String unitName) {
+		this.unitName = unitName;
+	}
+}
+
 public class RecipeDTO {
 
 	private int recipeId;
 	private int authorUserId;
 	private String recipeTitle;
-	private ArrayList<String> recipeIngredients;
+	private ArrayList<Ingredient> ingredients;
 	private ArrayList<String> recipeSteps;
+	private String ingredientNames;
 	
 	public int getRecipeId() {
 		return recipeId;
@@ -28,11 +53,11 @@ public class RecipeDTO {
 	public void setRecipeTitle(String recipeTitle) {
 		this.recipeTitle = recipeTitle;
 	}
-	public ArrayList<String> getRecipeIngredients() {
-		return recipeIngredients;
+	public ArrayList<Ingredient> getIngredients() {
+		return ingredients;
 	}
-	public void setRecipeIngredients(ArrayList<String> recipeIngredients) {
-		this.recipeIngredients = recipeIngredients;
+	public void setIngredients(ArrayList<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 	public ArrayList<String> getRecipeSteps() {
 		return recipeSteps;
@@ -43,8 +68,16 @@ public class RecipeDTO {
 	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return recipeId + " " + recipeTitle + " " + recipeIngredients + " " + recipeSteps;
+		return recipeId + " " + recipeTitle + " " + ingredientsToString() + " " + recipeSteps;
+	}
+	
+	public String ingredientsToString() {
+		ingredientNames = null;
+		for (int i = 0; i < ingredients.size(); i++) {
+			Ingredient ing = ingredients.get(i);
+			ingredientNames.concat(ing.getName());
+		}
+		return ingredientNames;
 	}
 	
 	@Override
